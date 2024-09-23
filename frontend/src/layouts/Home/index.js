72232758e3df4cf81a618a7c8b7e939c652d0019
Matthew <matthew6303@gmail.com>
2024-09-23 15:14:47 -0400

    SCRUM-8 feat: mock login authentication & route protection

--- frontend/src/layouts/Home/index.js
+++ frontend/src/layouts/Home/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import routePath from 'constants/path';
+import { useAuth } from 'utils/hook/useAuth';
 
 import styles from './styles.module.scss';
 
 function HomePage() {
-	return <div className={styles.homeLayout}>Home</div>;
+	const { user, logout } = useAuth();
+
+	return (
+		<div className={styles.homeLayout}>
+			Home
+			<div>
+				{user ? (
+					<button type="button" onClick={logout}>
+						Logout
+					</button>
+				) : (
+					<Link to={routePath.login}>Login</Link>
+				)}
+			</div>
+			<Link to={routePath.profile}>Profile</Link>
+		</div>
+	);
 }
 
 export { HomePage };
